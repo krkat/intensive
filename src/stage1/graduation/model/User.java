f999f51a987793777f19a96ba3e22ec741ff0b8a
David Tagirov <david.tagirov.03@gmail.com>
2024-09-21 20:00:06 +0300

    Добавил поля, реализовал билдер и Comparable в классе Пользователь

--- src/stage1/graduation/model/User.java
+++ src/stage1/graduation/model/User.java
@@ -1,4 +1,57 @@
 package stage1.graduation.model;
 
-public class User {
+public class User implements Comparable<User> {
+    private final String name;
+    private final String password;
+    private final String email;
+
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public static class UserBuilder {
+        private String name;
+        private String password;
+        private String email;
+
+        public UserBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public User builder() {
+            return new User(name, password, email);
+        }
+    }
+
+    @Override
+    public int compareTo(User other) {
+        int nameComparison = this.name.compareTo(other.name);
+        if (nameComparison != 0) {
+            return nameComparison;
+        }
+
+        int passwordComparison = this.password.compareTo(other.password);
+        if (passwordComparison != 0) {
+            return passwordComparison;
+        }
+
+        return this.email.compareTo(other.email);
+    }
 }
