3416a27c30ed5455ae12b030071d0f5fffa513d6
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-09-23 17:01:57 +0300

    Add Console class

--- src/stage1/graduation/Main.java
+++ src/stage1/graduation/Main.java
@@ -1,24 +1,20 @@
 package stage1.graduation;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Main {
     static final int MIN_LENGTH = 1;
     static final int MAX_LENGTH = 10;
-    static final String POSITIVE_ANSWER = "yes";
-    static final String NEGATIVE_ANSWER = "no";
+    static final String YES = "yes";
+    static final String NO = "no";
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String userAnswer = POSITIVE_ANSWER;
-        boolean isWrongAnswer;
+        String userAnswer = YES;
+        boolean isCorrectInput;
         do {
-            if (POSITIVE_ANSWER.equals(userAnswer)) {
-                int type = askType(scanner);
-                int length = askLength(scanner);
-                int inputMethod = askInputMethod(scanner);
+            if (YES.equals(userAnswer)) {
+                int type = askType();
+                int length = askLength();
+                int inputMethod = askInputMethod();
 
                 // Ввод данных с использованием стратегии
                 /*InputData strategy = new InputData(inputMethod);
@@ -41,66 +37,49 @@ public class Main {
                 // дополнительное задание - сортировка по числовому полю
                 // вывод отсортированного массива
 
-                isWrongAnswer = false;
+                isCorrectInput = true;
             } else {
-                isWrongAnswer = true;
+                isCorrectInput = false;
             }
-            userAnswer = askToContinue(scanner, isWrongAnswer).toLowerCase();
-        } while (!NEGATIVE_ANSWER.equals(userAnswer));
-        System.out.println("Завершение программы.");
-        scanner.close();
+            userAnswer = askToContinue(isCorrectInput).toLowerCase();
+        } while (!NO.equals(userAnswer));
+        Console.println("Завершение программы.");
+        Console.close();
     }
 
-    private static int askType(Scanner scanner) {
-        System.out.println("1 - Автобус, 2 - Пользователь, 3 - Студент");
-        System.out.print("Введите тип объекта, с которым хотите работать: ");
-        return input(scanner, 1, 3);
+    private static int askType() {
+        Console.println("1 - Автобус, 2 - Пользователь, 3 - Студент");
+        Console.print("Введите тип объекта, с которым хотите работать: ");
+        return inputItem(1, 3);
     }
 
-    private static int input(Scanner scanner, int minBound, int maxBound) {
-        int inputNumber;
-        boolean isWrongAnswer = false;
-        do {
-            if (isWrongAnswer) {
-                System.out.printf("Ошибка! Введите число от %d до %d: ", minBound, maxBound);
-            }
-            inputNumber = inputInt(scanner);
-            isWrongAnswer = true;
-        } while (inputNumber < minBound || inputNumber > maxBound);
-        return inputNumber;
-    }
-
-    private static int inputInt(Scanner scanner) {
+    private static int inputItem(int minBound, int maxBound) {
         int item;
+        boolean isWrongInput = false;
         do {
-            try {
-                item = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.print("Ошибка! Введите корректное целое число: ");
-                continue;
-            } finally {
-                scanner.nextLine();
+            if (isWrongInput) {
+                Console.printf("Ошибка! Введите число от %d до %d: ", minBound, maxBound);
             }
-            break;
-        } while (true);
+            item = Console.readInt();
+            isWrongInput = true;
+        } while (item < minBound || item > maxBound);
         return item;
     }
 
-    private static int askLength(Scanner scanner) {
-        System.out.printf("Введите длину массива от %d до %d: ", MIN_LENGTH, MAX_LENGTH);
-        int length = input(scanner, MIN_LENGTH, MAX_LENGTH);
-        return length;
+    private static int askLength() {
+        Console.printf("Введите длину массива от %d до %d: ", MIN_LENGTH, MAX_LENGTH);
+        return inputItem(MIN_LENGTH, MAX_LENGTH);
     }
 
-    private static int askInputMethod(Scanner scanner) {
-        System.out.println("1 - Ручной ввод, 2 - Генерация, 3 - Из файла");
-        System.out.print("Укажите способ ввода данных: ");
-        return input(scanner, 1, 3);
+    private static int askInputMethod() {
+        Console.println("1 - Ручной ввод, 2 - Генерация, 3 - Из файла");
+        Console.print("Укажите способ ввода данных: ");
+        return inputItem(1, 3);
     }
 
-    private static String askToContinue(Scanner scanner, boolean isWrongAnswer) {
-        System.out.print(!isWrongAnswer ? "Хотите продолжить? [yes/no]: " :
+    private static String askToContinue(boolean isCorrectInput) {
+        Console.print(isCorrectInput ? "Хотите продолжить? [yes/no]: " :
                 "Введите корректный ответ [yes/no]: ");
-        return scanner.nextLine();
+        return Console.readString();
     }
 }
